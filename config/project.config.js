a76c06a4aa50778b657744a5da6dce7f1a0c631b
Ballard <lappb172@gmail.com>
2017-11-08 01:37:27 +0700

    change port && update react-zet-com

--- config/project.config.js
+++ config/project.config.js
@@ -25,7 +25,7 @@ const config = {
   // Server Configuration
   // ----------------------------------
   server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
-  server_port : process.env.PORT || 5003,
+  server_port : process.env.PORT || 5006,
 
   // ----------------------------------
   // Compiler Configuration
